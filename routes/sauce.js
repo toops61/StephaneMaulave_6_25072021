718e4f4366ae8b3615b8619a507a127c3de90bdd
Toops61 <toops61@gmail.com>
2021-08-14 11:23:43 +0200

    fin P6

--- routes/sauce.js
+++ routes/sauce.js
@@ -11,6 +11,6 @@ router.put('/:id', auth, multer, sauceCtrl.modifySauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
 router.get('/', auth, sauceCtrl.getAllSauces);
-router.post('/:id/like', auth, multer, sauceCtrl.likeOneSauce);
+router.post('/:id/like', auth, sauceCtrl.likeOneSauce);
 
 module.exports = router;
